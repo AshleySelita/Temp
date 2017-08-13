36f3355855507dd6bc24365188ec25f5ba81e60e
mzq667 <x8888888888___0@163.com>
2017-08-13 21:17:21 +0800

    common update

---
+++ 联合权值.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <cctype>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+typedef long long ll;
+const int MAXN = 200010;
+const int MOD = 10007;
+struct node {
+	int edge[11];
+}a[MAXN];
+int n, u, v;
+int main() {
+	scanf("%d", &n);
+	for (int i = 1; i <= n; ++i) {
+		scanf("%d%d", &u, &v);
+	}
+}
